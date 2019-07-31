e5d27843bb163a9c63e636701cb8747f46497860
hongch <hongch@ufotosoft.com>
2019-07-31 11:43:26 +0800

    **[提交代码]

---
+++ data/conf/extra/webconfig.php
@@ -0,0 +1,15 @@
+<?php return [
+                    'admin_title'=>'海洋世界',
+                    'title'=>'海洋世界',
+                    'keywords'=>'海洋世界',
+                    'logo'=>'/upload/admin/thumb/79/ca50c9a3dc07227ae415fa7769e4b7964eb301.png',
+                    'desc'=>'海洋世界',
+                    'icp'=>'',
+                    'code'=>'',
+                    'domain'=>'http://www.91ye.top',
+                    'port_cache_time'=>'',
+                    'openRedisQueue'=>'1',
+                    'iosDownloadlink'=>'#',
+                    'androidDownloadlink'=>'http://www.91ye.top/app',
+                    'customer_service'=>'http://www.91ye.top'
+                ];
